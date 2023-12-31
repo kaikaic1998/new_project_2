dbe8e2cb8e05cd1f9605ebfb69b2c5ad74c1f96e
kaikaic1998 <kainianc@gmail.com>
2023-12-31 11:31:15 +0900

    modify code

--- try.py
+++ try.py
@@ -2,12 +2,17 @@ import os
 import leafmap
 from samgeo import SamGeo, tms_to_geotiff, get_basemaps
 from PIL import Image
+from typing import Callable, Optional
+from torch import Tensor
 
 from torchgeo.datasets import BigEarthNet
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 
 import os
+
+# reference
+# https://pytorch.org/blog/geospatial-deep-learning-with-torchgeo/
 
 # use when download torchgeo dataset
 import ssl
@@ -28,12 +33,30 @@ def read_tif():
     jpeg_image.show()
     print(jpeg_image)
 # read_tif()
+        
+
+dataset = BigEarthNet(root='./BigEarthNet/', bands='s2', download=False, checksum=False)
 
 
-dataset = BigEarthNet(root='./dataset/', bands='s2', download=True, checksum=True)
-# dataloader = DataLoader(dataset, batch_size=1)
-# print(len(dataloader))
+print('type: ', type(dataset[0]))
+print('keys: ', dataset[0].keys())
+print('label size', dataset[0]['label'].size())
+# print(dataset.folders[0]) # path
+# print(dataset.class_sets[19]) # label names
+print('image size: ', dataset[0]['image'].size())
 
-# print(dataloader)
+# get label names
+label_mask = dataset[0]['label']
+label_name = dataset._onehot_labels_to_names(label_mask)
+print('label name: ', label_name, '\n')
 
-print(len(dataset))
+# dataloader
+dataloader = DataLoader(dataset, batch_size=1)
+for batch in dataloader:
+    print(batch['image'].size())
+    print(batch['label'])
+    break
+
+# plot dataset image
+fig = dataset.plot(dataset[0])
+plt.show()
